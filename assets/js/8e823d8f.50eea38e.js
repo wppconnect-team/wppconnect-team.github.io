c97b3c5dc5ecfaf90368924643fefd90b501aaf5
edgardmessias <edgardmessias@users.noreply.github.com>
2022-06-28 23:28:45 +0000

    deploy: fa89271a797a82cbdf9bca11af1112b5ecb6aa75

--- assets/js/8e823d8f.50eea38e.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwppconnect_team_github_io=self.webpackChunkwppconnect_team_github_io||[]).push([[1142],{46179:e=>{e.exports=JSON.parse('{"permalink":"/blog/tags/browser","page":1,"postsPerPage":10,"totalPages":1,"totalCount":8,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
