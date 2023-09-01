fdc27ddfb7dd9289edbb40b714ecb595ab9a46de
edgardmessias <edgardmessias@users.noreply.github.com>
2023-09-01 22:34:05 +0000

    deploy: 92992c4e3f710925733cca9749ea81f579f91314

---
+++ assets/js/9f0ef2b0.76409d2c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwppconnect_team_github_io=self.webpackChunkwppconnect_team_github_io||[]).push([[2308],{78455:e=>{e.exports=JSON.parse('{"permalink":"/blog/page/12","page":12,"postsPerPage":10,"totalPages":15,"totalCount":141,"previousPage":"/blog/page/11","nextPage":"/blog/page/13","blogDescription":"Blog","blogTitle":"Blog"}')}}]);
---
+++ pt-BR/assets/js/eddf6a1e.09cb103c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwppconnect_team_github_io=self.webpackChunkwppconnect_team_github_io||[]).push([[2406],{23978:e=>{e.exports=JSON.parse('{"permalink":"/pt-BR/blog/tags/browser/page/7","page":7,"postsPerPage":10,"totalPages":7,"totalCount":63,"previousPage":"/pt-BR/blog/tags/browser/page/6","blogDescription":"Blog","blogTitle":"Blog"}')}}]);
