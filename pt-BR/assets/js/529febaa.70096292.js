e2ea9f7aeb7f71cfb5d718062e22e5605f5071a6
edgardmessias <edgardmessias@users.noreply.github.com>
2023-12-25 23:20:38 +0000

    deploy: a0ea7908c40031163ba037ca360ad08b0c7dffa8

--- pt-BR/assets/js/529febaa.70096292.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwppconnect_team_github_io=self.webpackChunkwppconnect_team_github_io||[]).push([[47763],{72764:e=>{e.exports=JSON.parse('{"permalink":"/pt-BR/blog/tags/lib/page/4","page":4,"postsPerPage":10,"totalPages":4,"totalCount":35,"previousPage":"/pt-BR/blog/tags/lib/page/3","blogDescription":"Blog","blogTitle":"Blog"}')}}]);
