30338d4f9035700be888f4580493cb8d3a54f89f
icleitoncosta <icleitoncosta@users.noreply.github.com>
2024-10-24 23:28:59 +0000

    deploy: 79a5bb273ac542b4bcd098fe2ea3223297533a4e

---
+++ assets/js/6c0907d7.af54a8b6.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwppconnect_team_github_io=self.webpackChunkwppconnect_team_github_io||[]).push([[6326],{81621:e=>{e.exports=JSON.parse('{"permalink":"/blog/tags/javascript/page/22","page":22,"postsPerPage":10,"totalPages":22,"totalCount":216,"previousPage":"/blog/tags/javascript/page/21","blogDescription":"Blog","blogTitle":"Blog"}')}}]);
---
+++ pt-BR/assets/js/04beb03d.b94d700f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwppconnect_team_github_io=self.webpackChunkwppconnect_team_github_io||[]).push([[23650],{81243:e=>{e.exports=JSON.parse('{"permalink":"/pt-BR/blog/page/22","page":22,"postsPerPage":10,"totalPages":22,"totalCount":217,"previousPage":"/pt-BR/blog/page/21","blogDescription":"Blog","blogTitle":"Blog"}')}}]);
