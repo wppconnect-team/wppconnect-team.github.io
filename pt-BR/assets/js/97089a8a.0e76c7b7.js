9aa7d92fbcef7268fa68894edcf7a4047782e6fe
icleitoncosta <icleitoncosta@users.noreply.github.com>
2025-05-11 22:18:33 +0000

    deploy: 1cd7e4b4019030052bb511044f978e8072524a04

--- pt-BR/assets/js/97089a8a.0e76c7b7.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwppconnect_team_github_io=self.webpackChunkwppconnect_team_github_io||[]).push([[47404],{48595:t=>{t.exports=JSON.parse('{"label":"wppconnect","permalink":"/pt-BR/blog/tags/wppconnect","allTagsPath":"/pt-BR/blog/tags","count":251}')}}]);
