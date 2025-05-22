ecc52036cbcc51c07bffbe020f5efc21068d975a
icleitoncosta <icleitoncosta@users.noreply.github.com>
2025-05-22 12:33:30 +0000

    deploy: 26e31066fae7ff3b6bebb581df252ebbace4b7bb

--- assets/js/8f021b09.200c748d.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwppconnect_team_github_io=self.webpackChunkwppconnect_team_github_io||[]).push([[36917],{57433:e=>{e.exports=JSON.parse('{"permalink":"/blog/tags/browser/page/13","page":13,"postsPerPage":10,"totalPages":13,"totalCount":122,"previousPage":"/blog/tags/browser/page/12","blogDescription":"Blog","blogTitle":"Blog"}')}}]);
---
+++ assets/js/aa021b1a.73bec2f9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwppconnect_team_github_io=self.webpackChunkwppconnect_team_github_io||[]).push([[73680],{4305:e=>{e.exports=JSON.parse('{"permalink":"/blog/tags/wa-js/page/8","page":8,"postsPerPage":10,"totalPages":13,"totalCount":123,"previousPage":"/blog/tags/wa-js/page/7","nextPage":"/blog/tags/wa-js/page/9","blogDescription":"Blog","blogTitle":"Blog"}')}}]);
