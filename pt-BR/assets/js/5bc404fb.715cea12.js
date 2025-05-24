9ebeb7e9b75a0d53f866be04f614406096319c1e
icleitoncosta <icleitoncosta@users.noreply.github.com>
2025-05-24 01:09:31 +0000

    deploy: 7b4a735aedb8d4883a7c086783188959cfb72520

---
+++ pt-BR/assets/js/5bc404fb.715cea12.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwppconnect_team_github_io=self.webpackChunkwppconnect_team_github_io||[]).push([[62114],{99173:e=>{e.exports=JSON.parse('{"permalink":"/pt-BR/blog/tags/wppconnect/page/26","page":26,"postsPerPage":10,"totalPages":26,"totalCount":255,"previousPage":"/pt-BR/blog/tags/wppconnect/page/25","blogDescription":"Blog","blogTitle":"Blog"}')}}]);
--- pt-BR/assets/js/7dc7efcb.a271f8d7.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwppconnect_team_github_io=self.webpackChunkwppconnect_team_github_io||[]).push([[66166],{41929:t=>{t.exports=JSON.parse('{"permalink":"/pt-BR/blog/tags/wppconnect","page":1,"postsPerPage":10,"totalPages":26,"totalCount":254,"nextPage":"/pt-BR/blog/tags/wppconnect/page/2","blogDescription":"Blog","blogTitle":"Blog"}')}}]);
